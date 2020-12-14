23677e5120178cc828824c1866ad3f8927612713
Angel Palacios Mirafuentes <al221910938@gmail.com>
2020-12-13 22:07:34 -0600

    listo

--- resources/views/templates/carrito.blade.php
+++ resources/views/templates/carrito.blade.php
@@ -11,8 +11,8 @@
 	<meta charset="utf-8" />
 	<!-- <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" /> -->
 	<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
-     
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+
 	<link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
 
 </head>
@@ -81,7 +81,7 @@
 										<td>{{$item->name}}</td>
 										<td>{{$item->quantity}}</td>
 										<td>{{$item->price}}</td>
-										
+
 										<td>
 											<input type="hidden" name="total" value="{{$total=$total+ ($item->quantity * $item->price) }}">
 											<form action="{{route('cart.removeitem')}}" method="POST">
@@ -109,62 +109,55 @@
 
 							@endif
 
-							@if(empty(session('session_tipo')))
-							<button href="{{ route('registrarDireccion', ['id' => $item->id,'cantidad' => $total]) }}" class="button big" disabled>Comprar</button>
-							<h2>Para comprar Inicia Sesion</h2>
-							@endif
+							@if(!empty(session('session_tipo')))
+							<button href="{{ route('registrarDireccion', ['id' => $item->id,'cantidad' => $total]) }}" class="button big">Comprar</button>
+				<div align="center">
 
+					<h1>Pagar con Paypal</h1>
+
+					<script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
+					<!-- <script>paypal.Buttons().render('body');</script> -->
+					<script src="https://www.paypal.com/sdk/js?client-id=SB_CLIENT_ID">
+						// Required. Replace SB_CLIENT_ID with your sandbox client ID.
+					</script>
+
+
+
+					<div id="paypal-button-container"></div>
+
+
+					<script>
+						paypal.Buttons({
+							createOrder: function(data, actions) {
+								// This function sets up the details of the transaction, including the amount and line item details.
+								return actions.order.create({
+									purchase_units: [{
+										amount: {
+											value: '{{$total}}'
+										}
+									}]
+								});
+							},
+							onApprove: function(data, actions) {
+								// This function captures the funds from the transaction.
+								return actions.order.capture().then(function(details) {
+									// This function shows a transaction success message to your buyer.
+									alert('Transaction completed by ' + details.payer.name.given_name);
+								});
+							}
+						}).render('#paypal-button-container');
+						//This function displays Smart Payment Buttons on your web page.
+					</script>
+
+				</div>
+
+				@endif
 
 
 						</div>
 
 					</div>
 				</div>
-
-				<div align="center">
-
-				<h1>Pagar con Paypal</h1>
-
-				<script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
-                          <!-- <script>paypal.Buttons().render('body');</script> -->
-        <script
-          src="https://www.paypal.com/sdk/js?client-id=SB_CLIENT_ID"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
-        </script>
-   
-			
-
-			<div id="paypal-button-container"></div>
-
-			<!-- <script>
-				paypal.Buttons().render('#paypal-button-container');
-				// This function displays Smart Payment Buttons on your web page.
-			</script> -->
-
-			
-			<script>
-				paypal.Buttons({
-				createOrder: function(data, actions) {
-					// This function sets up the details of the transaction, including the amount and line item details.
-					return actions.order.create({
-					purchase_units: [{
-						amount: {
-						value: '{{$total}}'            }
-					}]
-					});
-				},
-				onApprove: function(data, actions) {
-					// This function captures the funds from the transaction.
-					return actions.order.capture().then(function(details) {
-					// This function shows a transaction success message to your buyer.
-					alert('Transaction completed by ' + details.payer.name.given_name);
-					});
-				}
-				}).render('#paypal-button-container');
-				//This function displays Smart Payment Buttons on your web page.
-			</script>
-
-			</div>
-
 
 			</div>
 		</div>
